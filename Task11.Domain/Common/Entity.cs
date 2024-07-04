5183382477a55f0b17692352d2ffbdd4e7f47576
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-04 20:12:23 +0300

    Add Entity

---
+++ Task11.Domain/Common/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Domain.Common
+{
+    public abstract class Entity<TId>(TId id) : IEquatable<Entity<TId>> where TId : notnull
+    {
+        public TId Id { get; protected set; }
+
+        public bool Equals(Entity<TId> other)
+        {
+            return Equals(other as object);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || obj is not Entity<TId>)
+            {
+                return false;
+            }
+
+            var entity = obj as Entity<TId>;
+
+            return Id.Equals(entity.Id);
+        }
+
+        public static bool operator ==(Entity<TId> left, Entity<TId> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity<TId> left, Entity<TId> right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
