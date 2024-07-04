4ec8599768ca7ee6827855ee663634bdd1c7b8c4
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-04 20:33:26 +0300

    Add FinanceOperationId

---
+++ Task11.Domain/FinanceOperationAggregate/ValueObjects/FinanceOperationId.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.Common;
+
+namespace Task11.Domain.FinanceOperationAggregate.ValueObjects
+{
+    public sealed class FinanceOperationId : ValueObject
+    {
+        private FinanceOperationId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; private set; }
+
+        public static FinanceOperationId CreateUniq() => new(Guid.NewGuid());
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
