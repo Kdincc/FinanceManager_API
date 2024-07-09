2a7dac39185159f90f5619f56cf2c9463e157517
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-09 23:17:13 +0300

    Add Validator

---
+++ Task11.Application/IncomeTypes/Commands/Create/CreateIncomeTypeCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.IncomeTypes.Commands.Create
+{
+    public sealed class CreateIncomeTypeCommandValidator : AbstractValidator<CreateIncomeTypeCommand>
+    {
+        public CreateIncomeTypeCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Description).NotEmpty();
+        }
+    }
+}
