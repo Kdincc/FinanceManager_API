9632f9f4099f6f926dddcb4f072146055b7e4d90
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-09 22:26:23 +0300

    Install MediatR

--- Task11.Infrastructure/DependencyInjection.cs
+++ Task11.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.IncomeFinanceOperation.Entities;
 using Task11.Infrastructure.Persistence;
+using Task11.Infrastructure.Persistence.Repos;
 
 namespace Task11.Infrastructure
 {
@@ -9,8 +12,9 @@ namespace Task11.Infrastructure
     {
         public static IServiceCollection AddInfarstructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<FinanceDbContext>(options => 
-            options.UseSqlServer(configuration.GetConnectionString("DbString"), b => b.MigrationsAssembly("Task11.Presentation")));
+            services.AddDbContext<FinanceDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DbString"), b => b.MigrationsAssembly("Task11.Presentation")));
+            
+            services.AddScoped<IRepository<IncomeType>, IncomeTypeRepository>();
 
             return services;
         }
