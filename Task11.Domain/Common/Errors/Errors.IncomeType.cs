ab0763da3002fb54c31b6c9906d80056b5ee1818
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-10 00:20:12 +0300

    Add Errors.IncomeType

---
+++ Task11.Domain/Common/Errors/Errors.IncomeType.cs
@@ -0,0 +1,25 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.IncomeFinanceOperation.Entities;
+
+namespace Task11.Domain.Common.Errors
+{
+    public static partial class Errors
+    {
+        public static class IncomeType
+        {
+            public static Error IncomeTypeNotFound => Error.NotFound(
+                code: "IncomeType.NotFound",
+                description: "Income type with that id not found!");
+
+            public static Error DuplicateIncomeType => Error.Conflict(
+                code: "IncomeType.DuplicateIncomeType", 
+                description: "Income type with same name and description already exists!")
+        }
+            
+    }
+}
