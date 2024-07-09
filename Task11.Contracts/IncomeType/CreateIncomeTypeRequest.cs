e76f7352b44132297f4169d63ebef929e01c48fd
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-09 19:43:39 +0300

    Add IncomeType Requests

---
+++ Task11.Contracts/IncomeType/CreateIncomeTypeRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Contracts.IncomeType
+{
+    public record CreateIncomeTypeRequest(string Name, string Description);
+}
