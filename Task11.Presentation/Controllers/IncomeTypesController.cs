13f2f269c23e6898360fdd244789043b3ec60d95
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-11 22:57:52 +0300

    Add IncomeTypesController

---
+++ Task11.Presentation/Controllers/IncomeTypesController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task11.Presentation.Controllers
+{
+    [Route("incomeTypes")]
+    public class IncomeTypesController(ISender sender) : ApiController
+    {
+        private readonly ISender _sender = sender;
+
+        [HttpGet("all")]
+        public IActionResult GetIncomeTypes() 
+        {
+
+
+            return Ok();
+        }
+    }
+}
