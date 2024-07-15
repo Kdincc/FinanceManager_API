418ccbbd87c64eeeb6c244928c71be795a7c7768
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 15:15:06 +0300

    Add UpdateExpenseTypeCommand

---
+++ Task11.Application/ExpenseTypes/Commands/Update/UpdateExpenseTypeCommand.cs
@@ -0,0 +1,6 @@
+﻿using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+
+namespace Task11.Application.ExpenseTypes.Commands.Update
+{
+    public record UpdateExpenseTypeCommand(ExpenseTypeId ExpenseTypeId, string Name, string Description);
+}
