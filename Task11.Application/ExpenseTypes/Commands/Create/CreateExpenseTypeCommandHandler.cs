ec28fa8d84ce12c9bfbc0e6524cceead2ecd523e
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 13:48:11 +0300

    Add CreateExpenseTypeCommandHandler

---
+++ Task11.Application/ExpenseTypes/Commands/Create/CreateExpenseTypeCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.Common.Errors;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+using Task11.Domain.ExpenseType;
+
+namespace Task11.Application.ExpenseTypes.Commands.Create
+{
+    public sealed class CreateExpenseTypeCommandHandler(IRepository<ExpenseType, ExpenseTypeId> repository) : IRequestHandler<CreateExpenseTypeCommand, ErrorOr<ExpenseTypesResult>>
+    {
+        private readonly IRepository<ExpenseType, ExpenseTypeId> _repository = repository;
+
+        public async Task<ErrorOr<ExpenseTypesResult>> Handle(CreateExpenseTypeCommand request, CancellationToken cancellationToken)
+        {
+            ExpenseType expenseType = new(ExpenseTypeId.CreateUniq(), request.Name, request.Description);
+
+            var expenseTypes = await _repository.GetAllAsync(cancellationToken);
+
+            if(expenseTypes.Any(e => e.Name == expenseType.Name && e.Description == expenseType.Description)) 
+            {
+                return Errors.ExpenseType.DuplicateExpenseType;
+            }
+
+            await _repository.AddAsync(expenseType, cancellationToken);
+
+            return new ExpenseTypesResult(expenseType);
+        }
+    }
+}
