4a421458eb6b3762fa4073fffb6376b2ed6756cb
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 16:10:42 +0300

    Add GetExpenseTypesQuery

---
+++ Task11.Application/ExpenseTypes/Queries/GetExpenseTypes/GetExpenseTypesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+using Task11.Domain.ExpenseType;
+
+namespace Task11.Application.ExpenseTypes.Queries.GetExpenseTypes
+{
+    public sealed class GetExpenseTypesQueryHandler(IRepository<ExpenseType, ExpenseTypeId> repository) : IRequestHandler<GetExpenseTypesQuery, IEnumerable<ExpenseTypesResult>>
+    {
+        private readonly IRepository<ExpenseType, ExpenseTypeId> _repository = repository;
+
+        public async Task<IEnumerable<ExpenseTypesResult>> Handle(GetExpenseTypesQuery request, CancellationToken cancellationToken)
+        {
+            IReadOnlyCollection<ExpenseType> expenseTypes = await _repository.GetAllAsync(cancellationToken);
+
+            return expenseTypes.Select(e => new ExpenseTypesResult(e));
+        }
+    }
+}
