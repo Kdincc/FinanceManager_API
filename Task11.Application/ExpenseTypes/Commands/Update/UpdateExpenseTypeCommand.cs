52d709081f0d7692f23d16e20b8039298b13296e
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 16:02:31 +0300

    Add Update ExpenseType command handler

--- Task11.Application/ExpenseTypes/Commands/Update/UpdateExpenseTypeCommand.cs
+++ Task11.Application/ExpenseTypes/Commands/Update/UpdateExpenseTypeCommand.cs
@@ -1,6 +1,8 @@
-﻿using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+﻿using ErrorOr;
+using MediatR;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
 
 namespace Task11.Application.ExpenseTypes.Commands.Update
 {
-    public record UpdateExpenseTypeCommand(ExpenseTypeId ExpenseTypeId, string Name, string Description);
+    public record UpdateExpenseTypeCommand(ExpenseTypeId ExpenseTypeId, string Name, string Description) : IRequest<ErrorOr<ExpenseTypesResult>>;
 }
---
+++ Task11.Application/ExpenseTypes/Commands/Update/UpdateExpenseTypeCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using ErrorOr;
+using MediatR;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.Common.Errors;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+using Task11.Domain.ExpenseType;
+
+namespace Task11.Application.ExpenseTypes.Commands.Update
+{
+    public sealed class UpdateExpenseTypeCommandHandler(IRepository<ExpenseType, ExpenseTypeId> repository) : IRequestHandler<UpdateExpenseTypeCommand, ErrorOr<ExpenseTypesResult>>
+    {
+        private readonly IRepository<ExpenseType, ExpenseTypeId> _repository = repository;
+
+        public async Task<ErrorOr<ExpenseTypesResult>> Handle(UpdateExpenseTypeCommand request, CancellationToken cancellationToken)
+        {
+            ExpenseType expenseType = await _repository.GetByIdAsync(request.ExpenseTypeId, cancellationToken);
+
+            if (expenseType is null)
+            {
+                return Errors.ExpenseType.ExpenseTypeNotFound;
+            }
+
+            expenseType.ChangeName(request.Name);
+            expenseType.ChangeDescription(request.Description);
+
+            if (await HasSameExpenseType(_repository, expenseType))
+            {
+                return Errors.ExpenseType.DuplicateExpenseType;
+            }
+
+            return new ExpenseTypesResult(expenseType);
+        }
+
+        private async Task<bool> HasSameExpenseType(IRepository<ExpenseType, ExpenseTypeId> repository, ExpenseType expenseTypeToCheck)
+        {
+            await foreach (var expenseType in repository.GetAllAsAsyncEnumerable())
+            {
+                if (expenseType.HasSameNameAndDescription(expenseTypeToCheck))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
