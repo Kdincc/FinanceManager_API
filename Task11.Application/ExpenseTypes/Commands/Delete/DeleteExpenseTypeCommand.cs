9ee25fba273996b0955bb30a138f1035b2671276
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 15:07:05 +0300

    Add DeleteExpenseType command

---
+++ Task11.Application/ExpenseTypes/Commands/Delete/DeleteExpenseTypeCommand.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+
+namespace Task11.Application.ExpenseTypes.Commands.Delete
+{
+    public record DeleteExpenseTypeCommand(ExpenseTypeId ExpenseTypeId) : IRequest<ErrorOr<ExpenseTypesResult>>;
+}
