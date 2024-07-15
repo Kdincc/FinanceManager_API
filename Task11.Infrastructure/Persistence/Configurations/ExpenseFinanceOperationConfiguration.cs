4293de78f04eb3ff35e6b96459067aa6df47d257
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 22:53:04 +0300

    Cleaning code

--- Task11.Infrastructure/Persistence/Configurations/ExpenseFinanceOperationConfiguration.cs
+++ Task11.Infrastructure/Persistence/Configurations/ExpenseFinanceOperationConfiguration.cs
@@ -19,11 +19,14 @@ namespace Task11.Infrastructure.Persistence.Configurations
                 .ValueGeneratedNever()
                 .HasConversion(id => id.Value, value => ExpenseFinanceOperationId.Create(value));
 
-            builder.Property(x => x.Name).HasMaxLength(100);
+            builder.Property(x => x.Name)
+                .HasMaxLength(100);
 
-            builder.Property(x => x.Amount).HasConversion(amount => amount.Value, value => Amount.Create(value));
+            builder.Property(x => x.Amount)
+                .HasConversion(amount => amount.Value, value => Amount.Create(value));
 
-            builder.Property(x => x.ExpenseTypeId).HasConversion(id => id.Value, value => ExpenseTypeId.Create(value));
+            builder.Property(x => x.ExpenseTypeId)
+                .HasConversion(id => id.Value, value => ExpenseTypeId.Create(value));
 
             builder.HasOne<ExpenseType>()
                 .WithMany()
--- Task11.Infrastructure/Persistence/Configurations/IncomeFinanceOperationConfiguration.cs
+++ Task11.Infrastructure/Persistence/Configurations/IncomeFinanceOperationConfiguration.cs
@@ -19,11 +19,14 @@ namespace Task11.Infrastructure.Persistence.Configurations
                 .ValueGeneratedNever()
                 .HasConversion(id => id.Value, value => IncomeFinanceOperationId.Create(value));
 
-            builder.Property(x => x.Name).HasMaxLength(100);
+            builder.Property(x => x.Name)
+                .HasMaxLength(100);
 
-            builder.Property(x => x.Amount).HasConversion(amount => amount.Value, value => Amount.Create(value));
+            builder.Property(x => x.Amount)
+                .HasConversion(amount => amount.Value, value => Amount.Create(value));
 
-            builder.Property(x => x.IncomeTypeId).HasConversion(id => id.Value, value => IncomeTypeId.Create(value));
+            builder.Property(x => x.IncomeTypeId)
+                .HasConversion(id => id.Value, value => IncomeTypeId.Create(value));
 
             builder.HasOne<IncomeType>()
                 .WithMany()
