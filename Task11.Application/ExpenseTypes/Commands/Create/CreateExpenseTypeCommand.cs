0644a940cdb3475e8c4d54960f64c625fcf65de0
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 13:42:04 +0300

    Add CreateExpenseTypeCommand

---
+++ Task11.Application/ExpenseTypes/Commands/Create/CreateExpenseTypeCommand.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.ExpenseTypes.Commands.Create
+{
+    public record CreateExpenseTypeCommand(string Name, string Description) : IRequest<ErrorOr<ExpenseTypesResult>>;
+}
