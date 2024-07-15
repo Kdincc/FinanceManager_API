2cf4c1a0012084d547a925be78c27d9621b8daa1
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 16:05:05 +0300

    Add UpdateExpenseTypeCommandValidator

---
+++ Task11.Application/ExpenseTypes/Commands/Update/UpdateExpenseTypeCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Task11.Domain.Common.Сonstants;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.ExpenseTypes.Commands.Update
+{
+    public sealed class UpdateExpenseTypeCommandValidator : AbstractValidator<UpdateExpenseTypeCommand>
+    {
+        public UpdateExpenseTypeCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstantst.OperationType.MaxNameLength);
+
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstantst.OperationType.MaxDescriptionLength);
+
+            RuleFor(p => p.ExpenseTypeId)
+                .NotEmpty();
+        }
+    }
+}
