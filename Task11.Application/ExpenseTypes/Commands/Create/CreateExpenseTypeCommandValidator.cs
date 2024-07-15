7e1a4285875e98e6243e10be95c95e36cc52efdd
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-15 15:05:33 +0300

    Add ExpenseTypeValidator

---
+++ Task11.Application/ExpenseTypes/Commands/Create/CreateExpenseTypeCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Task11.Domain.Common.Сonstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.ExpenseTypes.Commands.Create
+{
+    public sealed class CreateExpenseTypeCommandValidator : AbstractValidator<CreateExpenseTypeCommand>
+    {
+        public CreateExpenseTypeCommandValidator()
+        {
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstantst.OperationType.MaxNameLength);
+            RuleFor(e => e.Description)
+                .NotEmpty()
+                .MaximumLength(ValidationConstantst.OperationType.MaxDescriptionLength);
+        }
+    }
+}
