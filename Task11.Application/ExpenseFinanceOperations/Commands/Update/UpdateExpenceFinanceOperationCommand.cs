8907a8ef2fea418fa8b38cac9b897792889a6924
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 18:57:41 +0300

    Add UpdateExpenceFinanceOperation

---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Update/UpdateExpenceFinanceOperationCommand.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using MediatR;
+using Task11.Domain.Common.ValueObjects;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+using Task11.Domain.ExpenseType.ValueObjects;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Update
+{
+    public record UpdateExpenceFinanceOperationCommand(
+        ExpenseFinanceOperationId ExpenseFinanceOperationId,
+        DateTime Date,
+        ExpenseTypeId ExpenceTypeId,
+        Amount Amount,
+        string Name) : IRequest<ErrorOr<ExpenseFinanceOperationResult>>;
+}
