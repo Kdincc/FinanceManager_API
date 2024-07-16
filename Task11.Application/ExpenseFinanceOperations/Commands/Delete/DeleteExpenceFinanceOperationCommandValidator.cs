5302a9d5c33de9219d107aac1df6f413cb563831
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 18:53:53 +0300

    Add DeleteExpenceFinanceOperationCommandValidator

---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Delete/DeleteExpenceFinanceOperationCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Delete
+{
+    public sealed class DeleteExpenceFinanceOperationCommandValidator : AbstractValidator<DeleteExpenceFinanceOperationCommand>
+    {
+        public DeleteExpenceFinanceOperationCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
