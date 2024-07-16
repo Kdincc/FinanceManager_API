8ee39850b5d75a43f05fa151cf91f15632bd88cc
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 19:21:05 +0300

    Remove responses

--- Task11.Contracts/IncomeType/CreateIncomeTypeResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Task11.Contracts.IncomeType
-{
-    public record CreateIncomeTypeResponse(Guid Id, string Name, string Description);
-}
