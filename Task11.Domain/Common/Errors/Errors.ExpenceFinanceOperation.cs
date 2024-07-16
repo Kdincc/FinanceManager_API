e709d4a9b8aa298e1a9c0d9442999b4b0356d290
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 18:25:35 +0300

    Add Errors.ExpenceFinanceOperation

---
+++ Task11.Domain/Common/Errors/Errors.ExpenceFinanceOperation.cs
@@ -0,0 +1,19 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Domain.Common.Errors
+{
+    public static partial class Errors
+    {
+        public static class ExpenceFinanceOperation
+        {
+            public static Error ExpenceFinanceOperationNotFound => Error.NotFound(
+                code: "ExpenceFinanceOperation.NotFound",
+                description: "Expence finance operation with that id not found!");
+        }
+    }
+}
