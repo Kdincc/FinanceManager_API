a53d686762729b8431cd9d991aad24c1ce1ae984
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 17:58:13 +0300

    Add CreateExpenseFinanceOpearion

---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Create/CreateExpenseFinanaceOperation.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Task11.Domain.Common.ValueObjects;
+using Task11.Domain.ExpenseType.ValueObjects;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Create
+{
+    public record CreateExpenseFinanaceOperation(
+        DateTime Date,
+        ExpenseTypeId ExpenseTypeId,
+        Amount Amount,
+        string Name) : IRequest<ErrorOr<ExpenseFinanceOperationResult>>;
+}
---
+++ Task11.Application/ExpenseFinanceOperations/ExpenseFinanceOperationResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.ExpenseFinanceOperation;
+
+namespace Task11.Application.ExpenseFinanceOperations
+{
+    public record ExpenseFinanceOperationResult(ExpenseFinanceOperation FinanceOperation);
+}
