d6ed2a0ab47caebb40703bd127208ae91dba279b
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 19:25:18 +0300

    Add CreateExpenceFinanceOperationRequest

---
+++ Task11.Contracts/ExpenceFinanceOperation/CreateExpenceFinanceOperationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Contracts.ExpenceFinanceOperation
+{
+    public record CreateExpenceFinanceOperationRequest(DateTime Date, string ExpenceTypeId, decimal Amount, string Name);
+}
