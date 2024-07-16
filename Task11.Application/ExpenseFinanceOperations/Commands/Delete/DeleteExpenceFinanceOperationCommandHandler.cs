4b42ee2b4cbc68fc2d358cecf7b0a7a5272aaa20
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 18:51:12 +0300

    Add DeleteExpenceFinanceOperationCommandHandler

---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Delete/DeleteExpenceFinanceOperationCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.Common.Errors;
+using Task11.Domain.ExpenseFinanceOperation;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Delete
+{
+    public sealed class DeleteExpenceFinanceOperationCommandHandler(IRepository<ExpenseFinanceOperation, ExpenseFinanceOperationId> repository) : IRequestHandler<DeleteExpenceFinanceOperationCommand, ErrorOr<ExpenseFinanceOperationResult>>
+    {
+        private readonly IRepository<ExpenseFinanceOperation, ExpenseFinanceOperationId> _repository = repository;
+
+        public async Task<ErrorOr<ExpenseFinanceOperationResult>> Handle(DeleteExpenceFinanceOperationCommand request, CancellationToken cancellationToken)
+        {
+            ExpenseFinanceOperation expenseFinanceOperation = await _repository.GetByIdAsync(request.Id, cancellationToken);
+
+            if (expenseFinanceOperation is null)
+            {
+                return Errors.ExpenceFinanceOperation.ExpenceFinanceOperationNotFound;
+            }
+
+            await _repository.DeleteAsync(expenseFinanceOperation, cancellationToken);
+
+            return new ExpenseFinanceOperationResult(expenseFinanceOperation);
+        }
+    }
+}
