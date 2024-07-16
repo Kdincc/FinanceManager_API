739a9a3d6922edecfbe7c2dfd981ff92bcfe1e78
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 19:18:01 +0300

    Add GetExpenceFinanceOperationsQueryHandler

---
+++ Task11.Application/ExpenseFinanceOperations/Queries/GetExpenceFinanceOperations/GetExpenceFinanceOperationsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.ExpenseFinanceOperation;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+
+namespace Task11.Application.ExpenseFinanceOperations.Queries.GetExpenceFinanceOperations
+{
+    public sealed class GetExpenceFinanceOperationsQueryHandler(IRepository<ExpenseFinanceOperation, ExpenseFinanceOperationId> repository) : IRequestHandler<GetExpenceFinanceOperationsQuery, IEnumerable<ExpenseFinanceOperationResult>>
+    {
+        private readonly IRepository<ExpenseFinanceOperation, ExpenseFinanceOperationId> _repository = repository;
+
+        public async Task<IEnumerable<ExpenseFinanceOperationResult>> Handle(GetExpenceFinanceOperationsQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<ExpenseFinanceOperation> operations = await _repository.GetAllAsync(cancellationToken);
+
+            return operations.Select(operation => new ExpenseFinanceOperationResult(operation));
+        }
+    }
+}
