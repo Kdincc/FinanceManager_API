e43b11d5e10022d0b3c707012a6678ac6181822f
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 18:46:46 +0300

    Add DeleteExpenceFinanceOperationCommand

---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Delete/DeleteExpenceFinanceOperationCommand.cs
@@ -0,0 +1,8 @@
+﻿using ErrorOr;
+using MediatR;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Delete
+{
+    public record DeleteExpenceFinanceOperationCommand(ExpenseFinanceOperationId Id) : IRequest<ErrorOr<ExpenseFinanceOperationResult>>;
+}
