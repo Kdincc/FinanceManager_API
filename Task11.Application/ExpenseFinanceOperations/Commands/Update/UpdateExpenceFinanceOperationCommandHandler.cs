93c8f7b2e21907ee3f9771496bff05f6d5998e96
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-16 19:09:02 +0300

    Add    UpdateExpenceFinanceOperationCommandValidator

---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Update/UpdateExpenceFinanceOperationCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using ErrorOr;
+using MediatR;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.ExpenseFinanceOperation;
+using Task11.Domain.ExpenseFinanceOperation.ValueObjects;
+using Task11.Domain.Common.Errors;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Update
+{
+    public sealed class UpdateExpenceFinanceOperationCommandHandler(IRepository<ExpenseFinanceOperation, ExpenseFinanceOperationId> repository) : IRequestHandler<UpdateExpenceFinanceOperationCommand, ErrorOr<ExpenseFinanceOperationResult>>
+    {
+        private readonly IRepository<ExpenseFinanceOperation, ExpenseFinanceOperationId> _repository = repository;
+
+        public async Task<ErrorOr<ExpenseFinanceOperationResult>> Handle(UpdateExpenceFinanceOperationCommand request, CancellationToken cancellationToken)
+        {
+            ExpenseFinanceOperation expenseFinanceOperation = await _repository.GetByIdAsync(request.ExpenseFinanceOperationId, cancellationToken);
+
+            if (expenseFinanceOperation is null)
+            {
+                return Errors.ExpenceFinanceOperation.ExpenceFinanceOperationNotFound;
+            }
+
+            expenseFinanceOperation.Update(
+                request.Date,
+                request.ExpenceTypeId,
+                request.Amount,
+                request.Name);
+
+            await _repository.UpdateAsync(expenseFinanceOperation, cancellationToken);
+
+            return new ExpenseFinanceOperationResult(expenseFinanceOperation);
+        }
+    }
+}
---
+++ Task11.Application/ExpenseFinanceOperations/Commands/Update/UpdateExpenceFinanceOperationCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Task11.Domain.Common.Сonstants;
+
+namespace Task11.Application.ExpenseFinanceOperations.Commands.Update
+{
+    public sealed class UpdateExpenceFinanceOperationCommandValidator : AbstractValidator<UpdateExpenceFinanceOperationCommand>
+    {
+        public UpdateExpenceFinanceOperationCommandValidator()
+        {
+            RuleFor(x => x.Date)
+                .NotEmpty();
+
+            RuleFor(x => x.ExpenceTypeId)
+                .NotEmpty();
+
+            RuleFor(x => x.Amount)
+                .NotEmpty()
+                .Must(x => x.Value >= 0);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstantst.ExpenseFinanceOperation.MaxNameLength);
+
+            RuleFor(x => x.ExpenseFinanceOperationId)
+                .NotEmpty();
+        }
+    }
+}
