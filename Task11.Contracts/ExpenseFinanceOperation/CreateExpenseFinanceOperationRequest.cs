32f7c931cf53cda47d6aa723162d7cfb66a21f6a
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-17 21:41:21 +0300

    Fix naming

---
+++ Task11.Contracts/ExpenseFinanceOperation/CreateExpenseFinanceOperationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Contracts.ExpenseFinanceOperation
+{
+    public record CreateExpenseFinanceOperationRequest(DateTime Date, string ExpenseTypeId, decimal Amount, string Name);
+}
--- Task11.Presentation/Mappings/ExpenseFinanceOperationMappingConfiguration.cs
+++ Task11.Presentation/Mappings/ExpenseFinanceOperationMappingConfiguration.cs
@@ -1,19 +1,19 @@
 ﻿using Mapster;
 using Task11.Application.ExpenseFinanceOperations.Commands.Create;
-using Task11.Contracts.ExpenceFinanceOperation;
+using Task11.Contracts.ExpenseFinanceOperation;
 using Task11.Domain.Common.ValueObjects;
 using Task11.Domain.ExpenseType.ValueObjects;
 
 namespace Task11.Presentation.Mappings
 {
-    public sealed class ExpenceFinanceOperationMappingConfiguration : IRegister
+    public sealed class ExpenseFinanceOperationMappingConfiguration : IRegister
     {
         public void Register(TypeAdapterConfig config)
         {
-            config.ForType<CreateExpenceFinanceOperationRequest, CreateExpenseFinanaceOperationCommand>()
+            config.ForType<CreateExpenseFinanceOperationRequest, CreateExpenseFinanaceOperationCommand>()
                 .ConstructUsing(src => new CreateExpenseFinanaceOperationCommand(
                     src.Date,
-                    ExpenseTypeId.Create(Guid.Parse(src.ExpenceTypeId)),
+                    ExpenseTypeId.Create(Guid.Parse(src.ExpenseTypeId)),
                     Amount.Create(src.Amount),
                     src.Name));
         }
