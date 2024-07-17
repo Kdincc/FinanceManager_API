f0eb209cf481c39616d03feca523c24bd45414a9
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-17 21:43:50 +0300

    Add UpdateExpenseFinanceOperationRequest

---
+++ Task11.Contracts/ExpenseFinanceOperation/UpdateExpenseFinanceOperationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Contracts.ExpenseFinanceOperation
+{
+    public record UpdateExpenseFinanceOperationRequest(string ExpenseFinanceOperationId, string Name, string ExpenseTypeId, decimal Amount, DateTime Date);
+    
+}
