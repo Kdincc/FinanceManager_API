79c583fab901ed0a18a3a9fcc4a374436ecd343d
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-17 21:36:15 +0300

    Add ExpenceFinanceOperationMappingConfiguration

--- Task11.Presentation/Mappings/ExpenceFinanceOperationMappingConfiguration.cs
+++ Task11.Presentation/Mappings/ExpenceFinanceOperationMappingConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using Mapster;
 using Task11.Application.ExpenseFinanceOperations.Commands.Create;
 using Task11.Contracts.ExpenceFinanceOperation;
+using Task11.Domain.Common.ValueObjects;
+using Task11.Domain.ExpenseType.ValueObjects;
 
 namespace Task11.Presentation.Mappings
 {
@@ -9,7 +11,11 @@ namespace Task11.Presentation.Mappings
         public void Register(TypeAdapterConfig config)
         {
             config.ForType<CreateExpenceFinanceOperationRequest, CreateExpenseFinanaceOperationCommand>()
-                .Map(dest => dest.Date, src => src.Date);
+                .ConstructUsing(src => new CreateExpenseFinanaceOperationCommand(
+                    src.Date,
+                    ExpenseTypeId.Create(Guid.Parse(src.ExpenceTypeId)),
+                    Amount.Create(src.Amount),
+                    src.Name));
         }
     }
 }
