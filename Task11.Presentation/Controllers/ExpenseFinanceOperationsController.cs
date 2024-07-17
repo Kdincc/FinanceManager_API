ef395c88eb0766b7adac90c71288bac80dc30cae
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-17 21:54:19 +0300

    Add ExpenseFinanceOperationsController and GetExpenseFinanceOperations

---
+++ Task11.Presentation/Controllers/ExpenseFinanceOperationsController.cs
@@ -0,0 +1,25 @@
+﻿using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task11.Application.ExpenseFinanceOperations;
+using Task11.Application.ExpenseFinanceOperations.Queries.GetExpenceFinanceOperations;
+
+namespace Task11.Presentation.Controllers
+{
+    [Route("expenses/operations")]
+    public class ExpenseFinanceOperationsController(ISender sender, IMapper mapper) : ApiController
+    {
+        private readonly ISender _sender = sender;
+        private readonly IMapper _mapper = mapper;
+
+        [HttpGet("all")]
+        [ProducesResponseType<IEnumerable<ExpenseFinanceOperationResult>>(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetExpenseFinanceOperations(CancellationToken cancellationToken)
+        {
+            IEnumerable<ExpenseFinanceOperationResult> results = await _sender.Send(new GetExpenceFinanceOperationsQuery(), cancellationToken);
+
+            return Ok(results);
+        }
+    }
+}
