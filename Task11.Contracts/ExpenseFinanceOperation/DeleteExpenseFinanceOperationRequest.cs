af86fa1b451230f79502a6fddbed64de0a4cb9d4
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-17 21:45:07 +0300

    Add DeleteExpenseFinanceOperation

---
+++ Task11.Contracts/ExpenseFinanceOperation/DeleteExpenseFinanceOperationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Contracts.ExpenseFinanceOperation
+{
+    public record DeleteExpenseFinanceOperationRequest(string ExpenseFinanceOperationId);
+    
+}
