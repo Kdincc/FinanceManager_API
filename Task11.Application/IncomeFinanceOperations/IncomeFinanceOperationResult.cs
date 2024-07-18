819225f4cb04495c155d6416c1c6bae184011566
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 14:05:01 +0300

    Add IncomeFinanceOperationResult

---
+++ Task11.Application/IncomeFinanceOperations/IncomeFinanceOperationResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.IncomeFinanceOperation;
+
+namespace Task11.Application.IncomeFinanceOperations
+{
+    public record IncomeFinanceOperationResult(IncomeFinanceOperation IncomeFinanceOperation);
+}
