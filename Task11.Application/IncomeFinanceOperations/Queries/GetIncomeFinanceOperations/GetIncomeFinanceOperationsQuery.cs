b191bea0046e9050a17f5ea39a1284657f7c6b62
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 15:06:30 +0300

    GetIncomeFinanceOperationsQueryHandler

---
+++ Task11.Application/IncomeFinanceOperations/Queries/GetIncomeFinanceOperations/GetIncomeFinanceOperationsQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.IncomeFinanceOperations.Queries.GetIncomeFinanceOperations
+{
+    public record GetIncomeFinanceOperationsQuery() : IRequest<IEnumerable<IncomeFinanceOperationResult>>;
+}
---
+++ Task11.Application/IncomeFinanceOperations/Queries/GetIncomeFinanceOperations/GetIncomeFinanceOperationsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Common.Persistance;
+using Task11.Domain.IncomeFinanceOperation;
+using Task11.Domain.IncomeFinanceOperation.ValueObjects;
+
+namespace Task11.Application.IncomeFinanceOperations.Queries.GetIncomeFinanceOperations
+{
+    public sealed class GetIncomeFinanceOperationsQueryHandler(IRepository<IncomeFinanceOperation, IncomeFinanceOperationId> repository) : IRequestHandler<GetIncomeFinanceOperationsQuery, IEnumerable<IncomeFinanceOperationResult>>
+    {
+        private readonly IRepository<IncomeFinanceOperation, IncomeFinanceOperationId> _repository = repository;
+
+        public async Task<IEnumerable<IncomeFinanceOperationResult>> Handle(GetIncomeFinanceOperationsQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<IncomeFinanceOperation> financeOperations = await _repository.GetAllAsync(cancellationToken);
+
+            return financeOperations.Select(financeOperation => new IncomeFinanceOperationResult(financeOperation));
+        }
+    }
+}
