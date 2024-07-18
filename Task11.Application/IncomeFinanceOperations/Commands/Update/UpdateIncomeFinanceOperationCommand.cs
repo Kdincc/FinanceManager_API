6af54a9c36b31907ca57a4d87e4ef1097408fd23
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 14:48:25 +0300

    Add UpdateIncomeFinanceOperationCommand

---
+++ Task11.Application/IncomeFinanceOperations/Commands/Update/UpdateIncomeFinanceOperationCommand.cs
@@ -0,0 +1,20 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.Common.ValueObjects;
+using Task11.Domain.IncomeFinanceOperation.ValueObjects;
+using Task11.Domain.IncomeType.ValueObjects;
+
+namespace Task11.Application.IncomeFinanceOperations.Commands.Update
+{
+    public record UpdateIncomeFinanceOperationCommand(
+        IncomeFinanceOperationId IncomeFinanceOperationId,
+        IncomeTypeId IncomeTypeId,
+        DateTime Date,
+        Amount Amount,
+        string Name) : IRequest<ErrorOr<IncomeFinanceOperationResult>>;
+}
