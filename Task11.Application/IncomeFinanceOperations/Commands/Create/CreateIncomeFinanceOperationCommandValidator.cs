aa2158985ca431d98ae77195d0d8c9b9220dc2f1
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 14:28:43 +0300

    Add CreateIncomeFinanceOperationCommandValidator

---
+++ Task11.Application/IncomeFinanceOperations/Commands/Create/CreateIncomeFinanceOperationCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Task11.Domain.Common.Сonstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.IncomeFinanceOperations.Commands.Create
+{
+    public sealed class CreateIncomeFinanceOperationCommandValidator : AbstractValidator<CreateIncomeFinanceOperationCommand>
+    {
+        public CreateIncomeFinanceOperationCommandValidator()
+        {
+            RuleFor(x => x.Date)
+                .NotEmpty();
+
+            RuleFor(x => x.IncomeTypeId)
+                .NotEmpty();
+
+            RuleFor(x => x.Amount)
+                .Must(x => x.Value >= 0);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.IncomeFinanceOperation.MaxNameLength);
+        }
+    }
+}
