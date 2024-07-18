59c1b52bbdd4c2c0646108ce05f43ff853851f70
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 14:08:42 +0300

    Add CreateIncomeFinanceOperationCommand

---
+++ Task11.Application/IncomeFinanceOperations/Commands/Create/CreateIncomeFinanceOperationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.Common.ValueObjects;
+using Task11.Domain.IncomeType.ValueObjects;
+
+namespace Task11.Application.IncomeFinanceOperations.Commands.Create
+{
+    public record CreateIncomeFinanceOperationCommand(DateTime Date, IncomeTypeId IncomeTypeId, Amount Amount, string Name);
+}
