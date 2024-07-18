8105620d3c32e05ba0b80139eb8f65d16d20d9ec
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 14:42:43 +0300

    Add DeleteIncomeFinanceOperationCommandValidator

---
+++ Task11.Application/IncomeFinanceOperations/Commands/Delete/DeleteIncomeFinanceOperationCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.IncomeFinanceOperations.Commands.Delete
+{
+    public sealed class DeleteIncomeFinanceOperationCommandValidator : AbstractValidator<DeleteIncomeFinanceOperationCommand>
+    {
+        public DeleteIncomeFinanceOperationCommandValidator()
+        {
+            RuleFor(x => x.IncomeFinanceOperationId)
+                .NotEmpty();
+        }
+    }
+}
