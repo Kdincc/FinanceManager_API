1f17dcd70fe466fc43a9f21aa2f8f2c215927fb1
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-18 14:31:27 +0300

    Add DeleteIncomeFinanceOperationCommand

---
+++ Task11.Application/IncomeFinanceOperations/Commands/Delete/DeleteIncomeFinanceOperationCommand.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Domain.IncomeFinanceOperation.ValueObjects;
+
+namespace Task11.Application.IncomeFinanceOperations.Commands.Delete
+{
+    public record DeleteIncomeFinanceOperationCommand(IncomeFinanceOperationId IncomeFinanceOperationId) : IRequest<ErrorOr<IncomeFinanceOperationResult>>;
+}
