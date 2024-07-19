04a480e054632db7aedd7a22721be5c60bb232b9
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-19 14:56:53 +0300

    Add GetDailyReportQuery

---
+++ Task11.Application/Reports/DailyReport/Queries/GetDailyReportQuery.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.Reports.DailyReport.Queries
+{
+    public record GetDailyReportQuery(DateOnly date) : IRequest<ErrorOr<DailyReport>>;
+}
