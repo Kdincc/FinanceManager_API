00a1e2fedcc3cbcf0333a2930ef1e13ce4d3eb8e
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-22 18:48:20 +0300

    Add DatePeriod

---
+++ Task11.Application/Reports/PeriodReport/DatePeriod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Application.Reports.PeriodReport
+{
+    public struct DatePeriod
+    {
+        public DatePeriod(DateOnly startDate, DateOnly endDate)
+        {
+            if (startDate > endDate)
+            {
+                throw new ArgumentException("Start date cannot be greater than end date.");
+            }
+
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public DateOnly StartDate { get; }
+        public DateOnly EndDate { get; }
+    }
+}
