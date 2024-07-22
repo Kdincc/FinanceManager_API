c915ee31408a355017651ebd681e3885d0f9f553
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-22 13:11:08 +0300

    Remove date errors

--- Task11.Domain/Common/Errors/Errors.IncomeFinanceOperation.cs
+++ Task11.Domain/Common/Errors/Errors.IncomeFinanceOperation.cs
@@ -9,10 +9,6 @@ namespace Task11.Domain.Common.Errors
             public static Error IncomeFinanceOperationNotFound => Error.NotFound(
                 code: "IncomeFinanceOperation.NotFound",
                 description: "Income finance operation with that id not found!");
-
-            public static Error IncomeFinanceOperationDateNotFound => Error.NotFound(
-                code: "IncomeFinanceOperation.DateNotFound",
-                description: "Income finance operation with that date not found!");
         }
     }
 }
