ffbe9197b38738d88ee4cb66711f369e484d1085
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-22 13:28:36 +0300

    Add ReportsControler

---
+++ Task11.Presentation/Controllers/ReportsController.cs
@@ -0,0 +1,27 @@
+﻿using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Task11.Application.Reports.DailyReport;
+using Task11.Application.Reports.DailyReport.Queries;
+using Task11.Contracts.DailyReport;
+
+namespace Task11.Presentation.Controllers
+{
+    [Route("reports")]
+    public class ReportsController(ISender sender, IMapper mapper) : ApiController
+    {
+        private readonly ISender _sender = sender;
+        private readonly IMapper _mapper = mapper;
+
+        [HttpGet("daily")]
+        [ProducesResponseType<DailyReport>(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetDailyReport(GetDailyReportRequest request)
+        {
+            var query = _mapper.Map<GetDailyReportQuery>(request);
+
+            var result = await _sender.Send(query);
+
+            return result.Match(Ok, Problem);
+        }
+    }
+}
