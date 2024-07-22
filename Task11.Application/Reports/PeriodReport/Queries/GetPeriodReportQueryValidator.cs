8e21332f0116dc06a34c83e546b585c08dee644c
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-22 19:25:19 +0300

    Add GetPeriodReportQueryValidator

---
+++ Task11.Application/Reports/PeriodReport/Queries/GetPeriodReportQueryValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Properties;
+
+namespace Task11.Application.Reports.PeriodReport.Queries
+{
+    public sealed class GetPeriodReportQueryValidator : AbstractValidator<GetPeriodReportQuery>
+    {
+        public GetPeriodReportQueryValidator()
+        {
+            RuleFor(x => x.StartDate)
+                .NotEmpty()
+                .Must(x => DateTime.TryParse(x, out _))
+                .WithMessage(ValidationErrorMessages.IncorrectDateFormat);
+
+            RuleFor(x => x.EndDate)
+                .NotEmpty()
+                .Must(x => DateTime.TryParse(x, out _))
+                .WithMessage(ValidationErrorMessages.IncorrectDateFormat);
+        }
+    }
+}
