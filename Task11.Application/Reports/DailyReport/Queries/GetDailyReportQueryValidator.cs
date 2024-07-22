a397959835d1cc3a2787761c94cab439e84f514a
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-22 17:52:43 +0300

    Add

---
+++ Task11.Application/Reports/DailyReport/Queries/GetDailyReportQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task11.Application.Properties;
+
+namespace Task11.Application.Reports.DailyReport.Queries
+{
+    public sealed class GetDailyReportQueryValidator : AbstractValidator<GetDailyReportQuery>
+    {
+        public GetDailyReportQueryValidator()
+        {
+            RuleFor(x => x.Date)
+                .NotEmpty()
+                .Must(x => DateTime.TryParse(x, out _))
+                .WithMessage(ValidationErrorMessages.IncorrectDateFormat);
+        }
+    }
+}
