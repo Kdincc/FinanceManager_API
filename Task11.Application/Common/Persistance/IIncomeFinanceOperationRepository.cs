dbd65e61b05405f131119c03cb5a325705fcceeb
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-07-28 17:52:40 +0300

    Remove IRepository

--- Task11.Application/Common/Persistance/IIncomeFinanceOperationRepository.cs
+++ Task11.Application/Common/Persistance/IIncomeFinanceOperationRepository.cs
@@ -1,9 +1,21 @@
-﻿using Task11.Domain.IncomeFinanceOperationAggregate;
+﻿using System.Security.Cryptography;
+using Task11.Domain.IncomeFinanceOperationAggregate;
 using Task11.Domain.IncomeFinanceOperationAggregate.ValueObjects;
 
 namespace Task11.Application.Common.Persistance
 {
-    public interface IIncomeFinanceOperationRepository : IRepository<IncomeFinanceOperation, IncomeFinanceOperationId>
+    public interface IIncomeFinanceOperationRepository
     {
+        public IAsyncEnumerable<IncomeFinanceOperation> GetAllAsAsyncEnumerable();
+
+        public Task<IncomeFinanceOperation> GetByIdAsync(IncomeFinanceOperationId id, CancellationToken cancellationToken);
+
+        public Task<IReadOnlyCollection<IncomeFinanceOperation>> GetAllAsync(CancellationToken cancellationToken);
+
+        public Task AddAsync(IncomeFinanceOperation entity, CancellationToken cancellationToken);
+
+        public Task UpdateAsync(IncomeFinanceOperation entity, CancellationToken cancellationToken);
+
+        public Task DeleteAsync(IncomeFinanceOperation entity, CancellationToken cancellationToken);
     }
 }
--- Task11.Application/Common/Persistance/IIncomeTypeRepository.cs
+++ Task11.Application/Common/Persistance/IIncomeTypeRepository.cs
@@ -1,9 +1,21 @@
-﻿using Task11.Domain.IncomeType;
+﻿using System.Security.Cryptography;
+using Task11.Domain.IncomeType;
 using Task11.Domain.IncomeType.ValueObjects;
 
 namespace Task11.Application.Common.Persistance
 {
-    public interface IIncomeTypeRepository : IRepository<IncomeType, IncomeTypeId>
+    public interface IIncomeTypeRepository
     {
+        public IAsyncEnumerable<IncomeType> GetAllAsAsyncEnumerable();
+
+        public Task<IncomeType> GetByIdAsync(IncomeTypeId id, CancellationToken cancellationToken);
+
+        public Task<IReadOnlyCollection<IncomeType>> GetAllAsync(CancellationToken cancellationToken);
+
+        public Task AddAsync(IncomeType entity, CancellationToken cancellationToken);
+
+        public Task UpdateAsync(IncomeType entity, CancellationToken cancellationToken);
+
+        public Task DeleteAsync(IncomeType entity, CancellationToken cancellationToken);
     }
 }
